7ab34369a4634fb9b797353d5ac684cc0069664e
Bryan Mihok <bmihok@veic.org>
2020-06-18 17:02:51 -0400

    Create Item Model With EntityFramework Support

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -7,5 +7,10 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
     }
 }
