1834efd66ca1e7f72a9737ceaac2e9d173c68c4f
Bryan Mihok <bmihok@veic.org>
2020-06-18 16:51:15 -0400

    decrement codegeneration.design and other stuff from next module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _context.Items.ToList();
+
+            return View("Index", items);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Item");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return null;
+        }
+    }
+}
